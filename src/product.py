84f24936338a61b2489c3ef1ffaa5039576f884e
Nikita Potapov <nekit132465@list.ru>
2025-04-09 18:58:53 +0300

    update class Product

--- src/product.py
+++ src/product.py
@@ -7,5 +7,24 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, new_product: dict):
+        return cls(
+            name=new_product.get("name"),
+            description=new_product.get("description"),
+            price=new_product.get("price"),
+            quantity=new_product.get("quantity"),
+        )
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price >= 0:
+            self.__price = new_price
+        print("Цена не должна быть нулевая или отрицательная")
